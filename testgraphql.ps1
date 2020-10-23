570c0a126639f319040e595a1b84c65c06eb0b70
Shane Krueger <shane@acdmail.com>
2020-10-23 01:41:33 -0400

    Update testgraphql.ps1

--- testgraphql.ps1
+++ testgraphql.ps1
@@ -1,10 +1,12 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ProgressPreference = "SilentlyContinue"
 $ErrorActionPreference = "Continue"
+$QUERY_TO_CHECK = $Env:QUERY_TO_CHECK -replace "`r","" -replace "`n",""
 
 "--- Testing GraphQL API: $($Env:URL_TO_CHECK) ---"
-"--- Testing Query: $($Env:QUERY_TO_CHECK) ---"
-$response5 = Invoke-RestMethod -Uri $Env:URL_TO_CHECK -Method Post -Body $Env:QUERY_TO_CHECK -ContentType 'application/json'
+"--- Testing Query desc: $($Env:QUERY_DESC) ---"
+"--- Testing Query: $($QUERY_TO_CHECK) ---"
+$response5 = Invoke-RestMethod -Uri $Env:URL_TO_CHECK -Method Post -Body $QUERY_TO_CHECK -ContentType 'application/json'
 $response5
 "------------------------------------"
 if (!$response5 -or $response5.errors) {
@@ -12,7 +14,7 @@ if (!$response5 -or $response5.errors) {
         {
             "channel": "#software",
             "username": "Azure Uptime Bot",
-            "text": "$($Env:URL_TO_CHECK) is currently offline; not returning graphql query data",
+            "text": "$($Env:URL_TO_CHECK) is currently offline retrieving $($Env:QUERY_DESC); not returning graphql query data",
             "icon_emoji":":bangbang:"
         }
 "@
