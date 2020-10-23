176e029314488b89769d67e874c0aede61cefa08
Shane Krueger <shane@acdmail.com>
2020-10-23 00:59:59 -0400

    Create test200.ps1

---
+++ test200.ps1
@@ -0,0 +1,20 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+"--- Testing $(Env:URL_TO_CHECK) ---"
+$response5 = Invoke-WebRequest -Uri ($Env:URL_TO_CHECK) -Method Get -UseBasicParsing
+$response5
+"-----------------------------------"
+if (!$response5 -or !$response5.StatusCode -eq 200) {
+    $BodyTemplate = @"
+        {
+            "channel": "#software",
+            "username": "Azure Uptime Bot",
+            "text": "$(Env:URL_TO_CHECK) is currently offline; not returning 200 status code",
+            "icon_emoji":":bangbang:"
+        }
+"@
+
+    Invoke-RestMethod -uri $Env:SLACK_CHANNEL_URI -Method Post -body $BodyTemplate -ContentType 'application/json'
+
+    exit 1
+}
