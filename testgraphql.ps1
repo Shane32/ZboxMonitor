aac34cacc6706816e04c02fa515989db30140bb7
Shane Krueger <shane@acdmail.com>
2020-10-23 01:24:27 -0400

    Create testgraphql.ps1

---
+++ testgraphql.ps1
@@ -0,0 +1,23 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "Continue"
+
+"--- Testing GraphQL API: $($Env:URL_TO_CHECK) ---"
+"--- Testing Query: $($Env:QUERY_TO_CHECK) ---"
+$response5 = Invoke-RestMethod -Uri $Env:URL_TO_CHECK -Method Post -Body $Env:QUERY_TO_CHECK -ContentType 'application/json'
+$response5
+"------------------------------------"
+if (!$response2 -or $response2.errors) {
+    $BodyTemplate = @"
+        {
+            "channel": "#software",
+            "username": "Azure Uptime Bot",
+            "text": "$($Env:URL_TO_CHECK) is currently offline; not returning graphql query data",
+            "icon_emoji":":bangbang:"
+        }
+"@
+
+    Invoke-RestMethod -uri $Env:SLACK_CHANNEL_URI -Method Post -body $BodyTemplate -ContentType 'application/json'
+
+    exit 1
+}
